f8ada8254096b0e9e620d7a571ec8cdeb0bcf97e
Nguyễn Đ. Tuân <tuanndph33203>
2025-12-12 17:16:48 +0700

    feat : add auth production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://inovice-prod-api.ansolution.vn/api',
   authencationUrl: 'https://authentsv.trananh.org/api',
-  microsoftClientKey: 'B3394396-4970-4459-812C-3AFFFA51C7E0',
+  microsoftClientKey: 'C6C1CA4F-DCF4-423D-85B8-8D57265BCC6F',
 };
